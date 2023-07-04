3412e764d8f0577671d8e5f41b962d0c967847c0
Mohammed Shafi <shafisnpni@gmail.com>
2023-07-04 22:32:54 +0400

    Made some changes on variables

--- project_dir/variables.tf
+++ project_dir/variables.tf
@@ -1,50 +1,22 @@
 variable "region" {
   
   type = list
-  default = ["us-east-1", "us-east-2"]
 }
 
-variable "project" {
+variable "project" {}
 
-    default = "popsocial"
-}
+variable "env" {}
 
-variable "env" {
+variable "cidr" {}
 
-    default = "prod"
-}
+variable "engine" {}
 
-variable "cidr" {
+variable "engine_version" {}
 
-    default = "172.16.0.0/16"
-}
+variable "db_name" {}
 
-variable "engine" {
-  
-  default = "aurora-mysql"
-}
+variable "master_usr" {}
 
-variable "engine_version" {
-  
-  default = "5.7.mysql_aurora.2.07.2"
-}
+variable "master_pswd" {}
 
-variable "db_name" {
-  
-  default = "mydb"
-}
-
-variable "master_usr" {
-  
-  default = "popsocial"
-}
-
-variable "master_pswd" {
-  
-  default = "Popsocial2023"
-}
-
-variable "db_class" {
-  
-  default = "db.r5.large"
-}
+variable "db_class" {}
